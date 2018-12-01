6bf7da4cfab4f265c4fb659a7fc1419953e39d83
Anibal Velarde <anibalvelarde@gmail.com>
2018-12-01 15:59:52 -0600

    Increased speed by using SortedList type.

--- calibrator/ConsoleApp1/Program.cs
+++ calibrator/ConsoleApp1/Program.cs
@@ -9,14 +9,14 @@ namespace ConsoleApp1
 {
     class Program
     {
-        private static List<int> _FreqList = new List<int>();
+        private static SortedList<int,int> _FreqList = new SortedList<int,int>();
         private static bool foundADup = false; 
         private static int loopCount = 0;
         private static int firstRepeat;
 
         static void Main(string[] args)
         {
-            _FreqList.Add(0);
+            _FreqList.Add(key:0, value:1);
             var listAsString = File.ReadAllLines(@"data\calibrationReadings.txt");
             var list = listAsString.Select(x => Convert.ToInt32(x)).ToList();
             var startingPoint = 0;
@@ -41,13 +41,15 @@ namespace ConsoleApp1
 
         private static void CheckFrequencyForRepeats(int newFreq)
         {
-            if (_FreqList.Any(x => x.Equals(newFreq)))
+            if (_FreqList.ContainsKey(newFreq))
             {
                 Console.WriteLine($"The frequency [{newFreq}] has been seen before.");
                 foundADup = true;
                 firstRepeat = newFreq;
+            } else
+            {
+                _FreqList.Add(key: newFreq, value: 0);
             }
-            _FreqList.Add(newFreq);
         }
     }
 }
---
+++ calibrator/ConsoleApp1/QuickSort.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class QuickSort
+    {
+
+        public int[] array;
+        public int len;
+
+        public void sort()
+        {
+            run(0, len - 1);
+        }
+
+        private void run(int left, int right)
+        {
+            int pivot, leftend, rightend;
+
+            leftend = left;
+            rightend = right;
+            pivot = array[left];
+
+            while (left < right)
+            {
+                while ((array[right] >= pivot) & amp; &amp; (left < right))
+                {
+                    right--;
+                }
+
+                if (left != right)
+                {
+                    array[left] = array[right];
+                    left++;
+                }
+
+                while ((array[left] >= pivot) & amp; &amp; (left < right))
+                {
+                    left++;
+                }
+
+                if (left != right)
+                {
+                    array[right] = array[left];
+                    right--;
+                }
+            }
+
+            array[left] = pivot;
+            pivot = left;
+            left = leftend;
+            right = rightend;
+
+            if (left < pivot) { sort(left, pivot - 1); }
+            if (right > pivot)
+            {
+                sort(pivot + 1, right);
+            }
+        }
+    }
+}
