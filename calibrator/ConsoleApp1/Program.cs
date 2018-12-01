060d92765657b298dbb1b3873db3c2472c3a513b
Anibal Velarde <anibalvelarde@gmail.com>
2018-12-01 14:00:58 -0600

    Solved challenge #1

--- calibrator/ConsoleApp1/Program.cs
+++ calibrator/ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,12 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            var listAsString = File.ReadAllLines(@"data\calibrationReadings.txt");
+            var list = listAsString.Select(x => Convert.ToInt32(x)).ToList();
+            var resultingFrequency = list.Aggregate(0, (int newFreq, int curFreq) => curFreq + newFreq);
+            Console.WriteLine($"Result {resultingFrequency.ToString()}");
+            Console.Write("Press Any Key...");
+            Console.ReadKey();
         }
     }
 }
