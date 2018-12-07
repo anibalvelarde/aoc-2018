c5b6431786cbb462748565d69f244e74acf5e49b
Anibal Velarde <anibalvelarde@gmail.com>
2018-12-06 19:19:33 -0600

    Initial commit. Day 5: Polymer Reader.

---
+++ Day5/PolymerReader.Tes/PolymerReaderTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PolymerReader.Tes
+{
+    [TestClass]
+    public class PolymerReaderTests
+    {
+        [TestMethod]
+        public void should_react_when_detecting_opposite_polarity()
+        {
+
+        }
+    }
+}
---
+++ Day5/PolymerReader.Tes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PolymerReader.Tes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PolymerReader.Tes")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aa53208e-18a1-4742-8a42-ea4cb7d608f4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
