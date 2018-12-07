cc69cc7a633ea64c42bc1c52eb1ecc153f9751e6
Anibal Velarde <anibalvelarde@gmail.com>
2018-12-06 21:19:49 -0600

    First algorithm... a bust.  It does not take into account what eliminations can take place as units in the polymer are eliminated.

--- Day5/PolymerReader.Tes/PolymerReaderTests.cs
+++ Day5/PolymerReader.Tes/PolymerReaderTests.cs
@@ -8,17 +8,73 @@ namespace PolymerReader.Test
     public class PolymerReaderTests
     {
         [TestMethod]
-        public void should_react_when_detecting_opposite_polarity()
+        public void should_react_when_detecting_opposite_polarity_for_2_units()
         {
             // arrange
             string polymer = "aA";
             var pr = new PolymerReader.Lib.PolymerReader(polymer);
             
             // act
-            string reactedPolymer = pr.React();
+            string reactedPolymer = pr.Trigger();
 
             // assert
             Assert.AreEqual("", reactedPolymer);
+        }
+
+        [TestMethod]
+        public void should_react_when_detecting_opposite_polarity_to_destroy_itself()
+        {
+            // arrange
+            string polymer = "abBA";
+            var pr = new PolymerReader.Lib.PolymerReader(polymer);
+
+            // act
+            string reactedPolymer = pr.Trigger();
+
+            // assert
+            Assert.AreEqual("", reactedPolymer);
+        }
+
+        [TestMethod]
+        public void should_not_react_when_polarities_are_ok()
+        {
+            // arrange 
+            string polymer = "abAB";
+            var pr = new PolymerReader.Lib.PolymerReader(polymer);
+
+            // act
+            string reactedPolymer = pr.Trigger();
+
+            // assert
+            Assert.AreEqual(polymer, reactedPolymer);
+        }
+
+        [TestMethod]
+        public void should_not_react_when_polarities_are_ok_on_larger_polymers()
+        {
+            // arrange 
+            string polymer = "aabAAB";
+            var pr = new PolymerReader.Lib.PolymerReader(polymer);
+
+            // act
+            string reactedPolymer = pr.Trigger();
+
+            // assert
+            Assert.AreEqual(polymer, reactedPolymer);
+        }
+
+        [TestMethod]
+        public void should_reduce_polymer_correctly()
+        {
+            // arrange 
+            string polymer = "dabAcCaCBAcCcaDA"; var expPolymer = "dabCBAcaDA";
+            var pr = new PolymerReader.Lib.PolymerReader(polymer);
+
+            // act
+            string reactedPolymer = pr.Trigger();
+
+            // assert
+            Assert.AreEqual(expPolymer, reactedPolymer);
         }
     }
 }
--- PolymerReader.Lib/PolymerReader.cs
+++ PolymerReader.Lib/PolymerReader.cs
@@ -8,16 +8,86 @@ namespace PolymerReader.Lib
 {
     public class PolymerReader
     {
+        private string _lowerUnits = "abcdefghijklmnopqrstuvwxyz";
+        private string _upperUnits = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private char _nullUnit = '\0';
+
         public string polymer { get; set; }
+        private Dictionary<char, char> _upperPolarity;
+        private Dictionary<char, char> _lowerPolarity;
         
         public PolymerReader(string polymer)
         {
+            LoadPolarityCheckers();
             this.polymer = polymer;
         }
 
-        public string React()
+        public string Trigger()
         {
-            return "";
+            var polymerFrag = this.polymer.ToCharArray();
+            var l = polymerFrag.Length;
+
+            for (int i = 0; i < l; i += 4)
+            {
+                char prev = polymer[i];
+                char current = i < (l - 1) ? polymerFrag[i + 1] : _nullUnit;
+                char next = i < (l - 2) ? polymerFrag[i + 2] : _nullUnit;
+                char nextNext = i < (l - 3) ? polymerFrag[i + 3] : _nullUnit;
+
+                if (annihilate(current,next))
+                {
+                    polymerFrag[i + 1] = _nullUnit;
+                    polymerFrag[i + 2] = _nullUnit;
+
+                    if (annihilate(prev, nextNext))
+                    {
+                        polymerFrag[i] = _nullUnit;
+                        polymerFrag[i + 3] = _nullUnit;
+                    }
+                } else if (annihilate(prev, current))
+                {
+                    polymerFrag[i] = _nullUnit;
+                    polymerFrag[i + 1] = _nullUnit;
+
+                    if (annihilate(next, nextNext))
+                    {
+                        polymerFrag[i + 2] = _nullUnit;
+                        polymerFrag[i + 3] = _nullUnit;
+                    }
+                } 
+            }
+
+            return new string(polymerFrag).Replace('\0'.ToString(), string.Empty);
+        }
+
+        private bool annihilate(char a, char b)
+        {
+            if (_lowerPolarity.ContainsKey(a))
+            {
+                return _lowerPolarity[a].Equals(b);
+            }
+            if (_upperPolarity.ContainsKey(b))
+            {
+                return _upperPolarity[b].Equals(a);
+            }
+            return false;
+        }
+
+        private void LoadPolarityCheckers()
+        {
+            var lower = this._lowerUnits.ToCharArray();
+            var upper = this._upperUnits.ToCharArray();
+
+            _lowerPolarity = lower.Select(lowUnit => lowUnit)
+                .ToDictionary(k => k, v => upper.First(x => {
+                    //Console.WriteLine($"x is [{x}] x.ToLower is [{x.ToString().ToLower()}] v is [{v}]");
+                    return x.ToString().ToLower().Equals(v.ToString());
+                }));
+            _upperPolarity = upper.Select(upUnit => upUnit)
+                .ToDictionary(k => k, v => lower.First(x => {
+                    //Console.WriteLine($"x is [{x}] x.ToLower is [{x.ToString().ToUpper()}] v is [{v}]");
+                    return x.ToString().ToUpper().Equals(v.ToString());
+                }));
         }
     }
 }
